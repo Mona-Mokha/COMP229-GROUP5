4355df443eb1e24035f690bd20eb0aba563ac86c
Mona-Mokha <mmokhala@my.centennialcollege.ca>
2025-12-05 00:13:03 -0500

    dekete donation test done

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,5 +1,16 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.config.cjs' }]
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+};
+module.exports = {
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.js'],
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy'
